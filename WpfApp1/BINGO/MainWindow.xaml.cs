33e1976f8caccd8f8167283f556e52aff4eca96d
Jesús Antonio Rodríguez Sánchez <jesus_rodriguez@ANPER-DESAR-L35>
2023-12-18 18:05:39 -0600

    Genera excel en el bingo y muestra numeros

--- WpfApp1/BINGO/MainWindow.xaml.cs
+++ WpfApp1/BINGO/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,17 +23,69 @@ namespace BINGO {
     /// </summary>
     public partial class MainWindow : Window {
         private List<int> numbers;
+        const int  max= 163;
         public MainWindow() {
             InitializeComponent();
             numbers = new List<int>();
+            //ExportToExcel(@"C:\opt\juego.xlsx");
+            numbers = new List<int>();
+        }
+
+        public void ExportToExcel(string filePath) {
+            Random random;
+            int number;
+
+            using (var spreadsheetDocument = SpreadsheetDocument.Create(filePath, SpreadsheetDocumentType.Workbook)) {
+                var workbookPart = spreadsheetDocument.AddWorkbookPart();
+                workbookPart.Workbook = new Workbook();
+
+                var worksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+                worksheetPart.Worksheet = new Worksheet();
+
+                var sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
+                var sheet = new Sheet { Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart), SheetId = 1, Name = "Personas" };
+                sheets.Append(sheet);
+
+                var sheetData = worksheetPart.Worksheet.AppendChild(new SheetData());
+
+                // Encabezados
+                var headerRow = new Row();
+                headerRow.AppendChild(new Cell(new CellValue("N1")));
+                headerRow.AppendChild(new Cell(new CellValue("N2")));
+                headerRow.AppendChild(new Cell(new CellValue("N3")));
+                sheetData.AppendChild(headerRow);
+
+                // Datos
+                //foreach (var person in people) {
+                while (true) {
+                    
+                    random = new Random();
+                    number = random.Next(1, max);
+
+                    if (numbers.Count(s => s == number) == 0) {
+                        lblNumber.Content = number.ToString();
+                        numbers.Add(number);
+                    }
+
+                    if (numbers.Count == (max - 1)) {
+                        break;
+                    }
+                }
+
+                for(int i=0; i< numbers.Count; i++) {
+                    var dataRow = new Row();
+                    dataRow.AppendChild(new Cell(new CellValue(numbers[i])));
+                    dataRow.AppendChild(new Cell(new CellValue(numbers[++i])));
+                    dataRow.AppendChild(new Cell(new CellValue(numbers[++i])));
+                    sheetData.AppendChild(dataRow);
+                }
+                
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             Random random;
             int number;
-            int max;
-
-            max = 110;
 
             while (true) {
                 random = new Random();
@@ -39,11 +94,13 @@ namespace BINGO {
                 if (numbers.Count(s => s == number) == 0) {
                     lblNumber.Content = number.ToString();
                     numbers.Add(number);
+                    lblNumbers.Text += number + ", ";
                     break;
                 }
 
-                if (numbers.Count== (max-1)) {
+                if (numbers.Count == (max - 1)) {
                     MessageBox.Show("El juego termino");
+                    break;
                 }
             }
         }
